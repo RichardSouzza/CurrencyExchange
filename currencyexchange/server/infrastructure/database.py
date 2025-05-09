cea690f41cc909ec3c61b8f637be824a777d89e8
Richard Souza <souzza.richard25@gmail.com>
2025-05-09 16:44:52 -0300

    🔊 feat: Improve logging

--- currencyexchange/server/infrastructure/database.py
+++ currencyexchange/server/infrastructure/database.py
@@ -1,6 +1,10 @@
+import logging
 from typing import Any
 
 from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorCollection
+
+
+logger = logging.getLogger(__name__)
 
 
 class Database:
@@ -13,15 +17,15 @@ class Database:
             self.client = AsyncIOMotorClient(self.uri)
             self.database = self.client.get_database(self.db_name)
             await self.client.admin.command("ping")
-            print("   SUCCESS   Connected to MongoDB successfully!")
+            logger.info("Connected to MongoDB successfully!")
         except Exception as error:
-            print(f"     ERROR   Failed to connect to MongoDB:\n    {error}")
+            logger.error(f"Failed to connect to MongoDB: \n{error}")
             raise error
 
     async def disconnect(self) -> None:
         if self.client:
             self.client.close()
-            print("       INFO   Database connection closed!")
+            logger.info("Disconnected from MongoDB successfully!")
 
     def get_collection(self, collection_name: str) -> AsyncIOMotorCollection[Any]:
         return self.database.get_collection(collection_name)
